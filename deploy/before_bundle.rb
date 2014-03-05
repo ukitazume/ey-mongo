96b8d2f25229d7ddc9a5f7b89433f5719af26d87
Yu Kitazume <u.kitazume@gmail.com>
2014-03-05 17:23:06 +0900

    ... miss force option

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,1 +1,1 @@
-run "ln -s -F #{config.shared_path}/config/mongoid.yml #{config.release_path}/config/mongoid.yml"
+run "ln -fs #{config.shared_path}/config/mongoid.yml #{config.release_path}/config/mongoid.yml"
