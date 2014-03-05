32d16021ebae23b679ab55aeabded619880e72b3
Yu Kitazume <u.kitazume@gmail.com>
2014-03-05 17:16:07 +0900

    debug

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,1 +1,2 @@
+puts "ln -s #{config.shared_path}/config/mongoid.yml #{config.release_path}/config/mongoid.yml"
 run "ln -s #{config.shared_path}/config/mongoid.yml #{config.release_path}/config/mongoid.yml"
