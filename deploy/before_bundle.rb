c2a1b67b930387cba9d6d51bc7d80757030f10a8
Yu Kitazume <u.kitazume@gmail.com>
2014-03-05 17:19:26 +0900

    force force!!

--- deploy/before_bundle.rb
+++ deploy/before_bundle.rb
@@ -1,2 +1,1 @@
-puts "ln -s #{config.shared_path}/config/mongoid.yml #{config.release_path}/config/mongoid.yml"
-run "ln -s #{config.shared_path}/config/mongoid.yml #{config.release_path}/config/mongoid.yml"
+run "ln -sF #{config.shared_path}/config/mongoid.yml #{config.release_path}/config/mongoid.yml"
